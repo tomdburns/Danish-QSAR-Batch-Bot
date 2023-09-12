0cdd5b7a8f452b3388986533f4863e6b2a213778
Burns <tom.daniel.burns@gmail.com>
2023-09-12 09:17:39 -0400

    v1.1.0
    - Added the modelxpaths.ini file which holds the xpaths for all models on the Endocrine tab.
    
    - Added a feature that allows the user to select the Endocrine model, or combination of endocrine models on the Endocrine tab by editing the models.ini file.
    
    - Fixed a bug that caused the code to crash on completion of calculations.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Troldmand v1.0.0 (Danish QSAR Batch Bot)
+# Troldmand v1.1.0 (Danish QSAR Batch Bot)
 
 Author: Tom Burns (github.com/tomdburns)
 
-Date  : August 31st, 2023
+Date  : September 11th, 2023
 
 Troldmand is a bot written in python to automate a batch mode for the Danish QSAR models:
 
@@ -34,6 +34,25 @@ troldmand will default to running the SMILES.txt in the code's directory
 when no input file is specified. The user can simply change the SMILES in
 this SMILES.txt file and double click the exe to run the code without
 needing the command line.
+
+## Models Available
+
+There are several models this code is compatible with run via the Danish QSAR Website:
+
+*ER alpha binding, all (human in vitro)           
+*ER alpha binding, balanced (human in vitro)      
+*ER alpha activation (human in vitro)             
+*ER Activation (in vitro, CERAPP data)            
+*AR inhibition (human in vitro)                   
+*AR binding (in vitro, CoMPARA data)              
+*AR activation (in vitro, CoMPARA data)           
+*AR inhibition (in vitro, CoMPARA data)           
+*Thyroperoxidase (TPO) inhibition QSAR1 (in vitro)
+*Thyroperoxidase (TPO) inhibition QSAR2 (in vitro)
+*Sodium/iodide symporter (NIS), higher sensitivity
+*Sodium/iodide symporter (NIS), higher specificity
+
+To select the models being run, change the models specified in models.ini
 
 ## Python dependencies (When running Python version)
 
--- troldmand.py
+++ troldmand.py
@@ -20,7 +20,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
 
-__VERSION__ = (1,0,0)
+__VERSION__ = (1,1,0)
 file_path   = '\\'.join(os.path.realpath(__file__).split('\\')[:-1])
 #file_path = '.'
 
@@ -68,10 +68,11 @@ class Options(object):
         self.ofile     = None
         self.limit     = False
         self.timeout   = 10
-        self.pause     = 10
+        self.pause     = 5
         self.url       = 'https://qsarmodels.food.dtu.dk/runmodel/index.html'
         self.models    = [_m.strip() for _m in open('%s\\models.ini' % file_path, 'r').readlines()]
         self.import_arguments()
+        self.import_modelpaths()
 
     def import_arguments(self):
         """imports the arguments"""
@@ -101,6 +102,21 @@ class Options(object):
                     self.show_help()
         if self.ofile is None:
             self.ofile = self.infile.split('.txt')[0] + '.csv'
+
+    def import_modelpaths(self):
+        """imports a full list of model paths"""
+        try:
+            _mfile = open('modelxpaths.ini', 'r').readlines()
+        except FileNotFoundError:
+            print('FATAL ERROR: modelxpaths.ini file not found!')
+            exit()
+        self.modelpaths = {}
+        for _line in _mfile:
+            if '=' not in _line:
+                continue
+            _model = _line.split('=')[0].strip()
+            _xpath = _line.split('=')[1].strip()
+            self.modelpaths[_model] = _xpath
 
     def show_help(self):
         """shows the help options"""
@@ -149,10 +165,10 @@ def format_results(results, options):
             exp = results[smi]['Experimental'][i]
             prd = results[smi]['Prediction'  ][i]
             prb = results[smi]['Probability' ][i]
-            rep = results[smi]['Report'      ][i]
+            #rep = results[smi]['Report'      ][i]
             out['%s - Experimental' % mod].append(exp)
             #out['%s - Probability'  % mod].append(prd)
-            out['%s - Prediction'   % mod].append(prb)
+            out['%s - Prediction'   % mod].append(prd)
             #out['%s - Report'       % mod].append(rep)
     return pd.DataFrame.from_dict(out)
 
@@ -182,8 +198,9 @@ def run(options, smiles, paths):
         # Select the models to run. Prototype version of this code
         # will just run all available models since it was faster to find
         # the "select all" checkbox in the elements for the site"
-        check = browser.find_element(By.XPATH, paths.get('check'))
-        check.click()
+        for model in options.models:
+            check = browser.find_element(By.XPATH, options.modelpaths[model])
+            check.click()
 
         print('\nInserting SMILES:', smi, '\n')
 
@@ -227,6 +244,8 @@ def run(options, smiles, paths):
                 sleep(tstep)
             else:
                 print('Calculation finished after %i seconds' % total)
+                endok = browser.find_element(By.XPATH, paths.get('endok'))
+                endok.click()
                 break
             if total >= timeout and options.limit:
                 print("Calculation timed out after %i seconds" % timeout)
@@ -240,21 +259,31 @@ def run(options, smiles, paths):
         table = browser.find_element(By.XPATH, paths.get('table'))
         data  = str(table.text)
         data  = data.split('\n')
-        refr  = []
-        sub   = []
-        for i, val in enumerate(data):
-            sub.append(val)
-            if (i+1) % per == 0:
-                refr.append(sub[:])
-                sub = []
+        mres, mcur = {}, None
+        for item in data[5:]:
+            if item in options.models:
+                mcur = item
+                mres[mcur] = []
+            elif mcur is None:
+                continue
+            else:
+                mres[mcur].append(item)
+        refr = {'Model': [], 'Experimental': [], 'Probability': [], 'Prediction': []}
+        for model in mres:
+            n = len(mres[model])
+            refr['Model'].append(model)
+            if n == 2:
+                refr['Experimental'].append(None)
+                refr['Probability'].append(mres[model][0])
+                refr['Prediction'].append(mres[model][1])
+            elif n == 3:
+                refr['Experimental'].append(mres[model][0])
+                refr['Probability'].append(mres[model][1])
+                refr['Prediction'].append(mres[model][2])
+        refr = pd.DataFrame.from_dict(refr)
         browser.close()
-
-        # Format the results array for later use
-        refr = np.array(refr)
-        cols = refr[0,:]
-        data = refr[1:,:]
-        refr = pd.DataFrame(data, columns=cols)
         results[smi] = refr
+
     return results
 
 
